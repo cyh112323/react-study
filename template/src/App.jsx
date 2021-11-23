2e307ecd2468811914dfa3b46f19b638dcc851e0
DESKTOP-904FIN7\tj-bu <yh4119@gmail.com>
2021-11-23 13:13:28 +0900

    백업파일 수정 , 기초개념 ,  01-1 state 기초문법

---
+++ template/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  let [_name, _setName] = useState('홍길동')  //let name = '홍길동'
+
+  let fn = (e) => {
+    //name = e.target.innerText
+    _setName(e.target.innerText)
+  }
+  return (
+    <>
+      <p>안녕하세요 {_name}입니다.</p>
+
+      <button onClick={fn}>홍길동</button>
+      <button onClick={fn}>아무개</button>
+    </>
+  );
+};
+
+export default App;
