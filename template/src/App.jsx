50429cfdb287d4706b094112feec626ad4ff1941
YH\yh4119 <yh4119@gmail.com>
2021-11-30 13:51:33 +0900

    2-6 배열 출력과 변경 정리

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const App = () => {
-  const [_arr, _setArr] = useState([])
-  const fnAdd = () => {
-    let key = Date.now()
-    _setArr([..._arr, key])
+  const [_arr,_setArr] = useState([])
+  const refInput = useRef()
+  const refInput2 = useRef()
+
+  const fnAdd = ()=>{
+    let name = refInput.current.value
+    let obj = {
+      id: Date.now(),
+      name
+    }
+    _setArr([..._arr, obj])
   }
-  const fnDel = (e) => {
-    let key = parseInt(e.target.getAttribute('data-n'))
-    let arr = _arr.filter((v)=>{
-      return v !== key
-    })
-    _setArr([...arr])
+  
+  const fnDel = (e)=>{
+    let delID = parseInt(e.target.getAttribute('data-n'))
+    let name = refInput2.current.value
+    
+    console.log(name,delID);
   }
   return (
     <>
-      <button onClick={fnAdd}>배열추가</button>
+      <p>이름을 추가하거나 제거하면 목록이 li객체로 출력됩니다</p>
+      <input ref={refInput} type="text" /> &nbsp;
+      <button onClick={fnAdd}>추가</button>
+      <br />
+      <input ref={refInput2} type="text" /> &nbsp;
+      <button onClick={fnDel}>제거</button>
       <hr />
-      {
-        _arr.map((v) => {
-          return (
-            <p key={v}>
-              {v}
-              <button onClick={fnDel} data-n={v}>삭제</button>
-            </p>
-          )
-        })
-      }
+      <ul>
+        {
+          _arr.map((v)=>{
+            return (
+              <li key={v.id}>
+                {v.name}
+              </li>
+            )
+          })
+        }
+      </ul> 
     </>
   );
 };
