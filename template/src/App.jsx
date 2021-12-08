d9b2e9130caa5c50e9e2a576bce28bec49ec4c6b
YH\yh4119 <yh4119@gmail.com>
2021-12-08 12:46:28 +0900

    router

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import CompFooter from './components/CompFooter';
+import CompHeader from './components/CompHeader';
+import CompHome from './components/CompHome';
+import CompAbout from './components/CompAbout';
+import CompPortfolio from './components/CompPortfolio';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      App
-    </>
+    <BrowserRouter>
+      <CompHeader />
+        <Routes>
+          <Route path="/" element={<CompHome />} />
+          <Route path="about" element={<CompAbout />} />
+          <Route path="portfolio" element={<CompPortfolio /> } />
+        </Routes>
+      <CompFooter />
+    </BrowserRouter>
   );
 };
 
