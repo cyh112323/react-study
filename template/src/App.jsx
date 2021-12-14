0efb0819d5fc77cebb3d85903e6b0e1efc07bbc2
YH\yh4119 <yh4119@gmail.com>
2021-12-14 13:50:48 +0900

    계산기만들기

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import CompFooter from './components/CompFooter';
-import CompHeader from './components/CompHeader';
-import CompHome from './components/CompHome';
-import CompAbout from './components/CompAbout';
-import CompPortfolio from './components/CompPortfolio';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import CompButtonContainer from './comp/CompButtonContainer';
+import CompOutput from './comp/CompOutput';
+
 
 const App = () => {
+  const [_result, _setResult] = useState()
   return (
-    <BrowserRouter>
-      <CompHeader />
-        <Routes>
-          <Route path="/" element={<CompHome />} />
-          <Route path="about" element={<CompAbout />} />
-          <Route path="portfolio" element={<CompPortfolio /> } />
-        </Routes>
-      <CompFooter />
-    </BrowserRouter>
+    <>
+      <main>
+        <CompOutput _result={_result} />
+        <CompButtonContainer _setResult={_setResult} />
+      </main>
+    </>
   );
 };
 
